11bf5bb02ade331a727d5417c702420553ab6cee
anonymous anonymous <kai.amory@kellogg.northwestern.edu>
2021-11-20 16:04:07 +0000

    Generate graphiti resources for reviews

---
+++ app/resources/review_resource.rb
@@ -0,0 +1,15 @@
+class ReviewResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :stars, :integer
+  attribute :review_text, :string
+  attribute :movie_id, :integer
+  attribute :user_id, :integer
+  attribute :director_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
