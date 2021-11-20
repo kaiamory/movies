954ac4cf3228b2cc2da77b6528a3a8a36d69dcf7
anonymous anonymous <kai.amory@kellogg.northwestern.edu>
2021-11-20 16:04:14 +0000

    Generate graphiti resources for actors

---
+++ app/resources/actor_resource.rb
@@ -0,0 +1,17 @@
+class ActorResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :dob, :date
+  attribute :bio, :string
+  attribute :image, :string
+  attribute :number_of_films, :integer
+  attribute :movie_id, :integer
+  attribute :director_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
